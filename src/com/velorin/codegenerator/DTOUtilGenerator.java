20865f4588ba7cfff25302f5abc2df9b631562fc
Amir Hooshangi <amirhoshangi@gmail.com>
2011-08-06 19:43:47 +0430

    cleaning code and changing methode signatures.

--- src/com/velorin/codegenerator/DTOUtilGenerator.java
+++ src/com/velorin/codegenerator/DTOUtilGenerator.java
@@ -4,8 +4,11 @@ import com.sun.codemodel.JBlock;
 import com.sun.codemodel.JClassAlreadyExistsException;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
-import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JPackage;
+import com.velorin.parser.BeanParser;
+import com.velorin.parser.JavaBeanParser;
 import com.velorin.utils.Util;
 import java.io.File;
 import java.io.IOException;
@@ -18,36 +21,54 @@ import java.util.ArrayList;
  * email: Amirhoshangi at Gmail
  */
 public class DTOUtilGenerator implements ClassGenerator {
-    
-    public void generateDTOClass(Class<?> clazz, ArrayList<Field> fields, String packageName) throws JClassAlreadyExistsException, IOException {
 
+//TODO: DTO class must implement implement serializable.
+    public void generateDTOClass(Class<?> bean, String packageName) throws JClassAlreadyExistsException, IOException {
+
+        BeanParser beanParser = new JavaBeanParser();
+        ArrayList<Field> fields = beanParser.getFields(bean);
         JCodeModel codeModel = new JCodeModel();
-        JDefinedClass definedClass = codeModel._class(clazz.getSimpleName()+"DTO");
+        JPackage jPackage = codeModel._package(packageName);
+        JDefinedClass definedClass = jPackage._class(bean.getSimpleName() + "DTO");
         for (int i = 0; i < fields.size(); i++) {
-            definedClass.field(4, fields.get(i).getType(), fields.get(i).getName());
+            definedClass.field(JMod.PRIVATE, fields.get(i).getType(), fields.get(i).getName());
         }
         Util.generateSetter(fields, definedClass);
         Util.generateGetter(fields, definedClass);
-        codeModel.build(new File(packageName));
+        codeModel.build(new File("/home/amir/Desktop"));
     }
 
-    public void generateDTOUtilClass(Class<?> toBeParsedClass, ArrayList<Field> fields, String packagePath) {
-    
-    
-    
-    }
+    public void generateDTOUtilClass(Class<?> bean, Class<?> beanDTO, String packageName) throws JClassAlreadyExistsException, IOException {
 
-
-    public void test(Class<?> clazz, ArrayList<Field> fields, File file) throws JClassAlreadyExistsException, IOException {
-        
+        BeanParser beanParser = new JavaBeanParser();
+        ArrayList<Field> fields = beanParser.getFields(bean);
         JCodeModel codeModel = new JCodeModel();
-        JDefinedClass definedClass = codeModel._class(clazz.getName()+"DTOConvertor");
-        String methodeName = "convert"+clazz.getSimpleName()+"to"+clazz.getSimpleName()+"DTO";
-        JMethod jMethod  = definedClass.method(1, int.class, methodeName);
-        JBlock jBlock = jMethod.body();
-        jBlock.directStatement("varName.setCode(100);");
-        file.mkdirs();
-        codeModel.build(file);
+        JPackage jPackage = codeModel._package(packageName);
+        JDefinedClass definedClass = jPackage._class(bean.getSimpleName() + "DTOUtil");
+        generateBeanToDTO(definedClass, beanDTO, bean, fields);
+        generateDTOtoBean(definedClass, bean, beanDTO, fields);
+        codeModel.build(new File("/home/amir/Desktop"));
     }
 
+    private void generateDTOtoBean(JDefinedClass definedClass, Class<?> bean, Class<?> beanDTO, ArrayList<Field> fields) {
+        JMethod jMethod = definedClass.method(JMod.PUBLIC | JMod.STATIC, bean, "convertDTOtoBean");
+        jMethod.param(0, beanDTO, "beanDTO");
+        JBlock jBlock = jMethod.body();
+        jBlock.directStatement(bean.getSimpleName() + " bean = new " + bean.getSimpleName() + "();");
+        for (int i = 0; i < fields.size(); i++) {
+            jBlock.directStatement("bean.set" + Util.uppercaseFirstCharacter(fields.get(i).getName()) + "(" + "beanDTO." + "get" + Util.uppercaseFirstCharacter(fields.get(i).getName()) + "()" + ");");
+        }
+        jBlock.directStatement("return bean;");
+    }
+
+    private void generateBeanToDTO(JDefinedClass definedClass, Class<?> beanDTO, Class<?> bean, ArrayList<Field> fields) {
+        JMethod jMethod = definedClass.method(JMod.PUBLIC | JMod.STATIC, beanDTO, "convertBeanToDTO");
+        jMethod.param(0, bean, "bean");
+        JBlock jBlock = jMethod.body();
+        jBlock.directStatement(beanDTO.getSimpleName() + " beanDTO = new " + beanDTO.getSimpleName() + "();");
+        for (int i = 0; i < fields.size(); i++) {
+            jBlock.directStatement("beanDTO.set" + Util.uppercaseFirstCharacter(fields.get(i).getName()) + "(" + "bean." + "get" + Util.uppercaseFirstCharacter(fields.get(i).getName()) + "()" + ");");
+        }
+        jBlock.directStatement("return beanDTO;");
+    }
 }
